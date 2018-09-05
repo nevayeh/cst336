967a66688958971c996a234b22a1a9fab621bcee
Neva Yeh <nyeh@csumb.edu>
2018-09-05 11:34:37 +0000

    initial commit, added composer.json, and index for assignments,etc

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+        <div>
+            <h1>Labs</h1>
+            <a href="/Labs/1">Lab 1</a>
+        </div>
+    </body>
+</html>
