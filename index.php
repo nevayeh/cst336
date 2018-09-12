b02ca2592488255f93dc89666e0cdb6043ce0395
Neva Yeh <nyeh@csumb.edu>
2018-09-12 20:58:43 +0000

    moved composer.json and index.php into cst336-repo folder

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+        <div>
+            <h1>Assignments</h1>
+            <div>
+                <a href="./cst336-repo/assignments/1">Assignment 1</a>
+            </div>
+            
+            <div>
+                <a href="./cst336-repo/assignments/2">Assignment 2</a>
+            </div>
+            
+            <br/>
+            
+            <hr>
+            
+            <h1>Homework</h1>
+            <div>
+                <a href="./cst336-repo/homework/1">Homework 1</a>
+            </div>
+            
+            <hr>
+            
+            <br/>
+            
+            <h1>Labs</h1>
+            <div>
+                <a href="./cst336-repo/labs/1">Lab 1</a>
+            </div>
+            
+        </div>
+    </body>
+</html>
