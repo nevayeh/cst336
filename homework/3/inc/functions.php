dadba1081998cae175eb9320144e6885ee5db47c
Neva Yeh <nyeh@csumb.edu>
2018-10-12 16:07:03 +0000

    completed homework 3

---
+++ homework/3/inc/functions.php
@@ -0,0 +1,197 @@
+<?php
+
+session_start();
+
+$_SESSION['numClasses'];
+$_SESSION['totalUnits'];
+$_SESSION['totalGradePoints'];
+$_SESSION['calculate'] = 0;
+
+function displayForm()
+{
+    echo '<fieldset>';
+        echo '<legend id="formLegend">GPA Calculator</legend>';
+        echo '<form method="post">';
+        
+        for($i = 0; $i < $_SESSION['numClasses']; $i++)
+        {
+            //Class name text field and label
+            //This information isn't processed, but is there for the users' own readability (to keep track of which courses they entered already)
+            echo '<label for="className' . $i . '"> (' . ($i+1) . ') Class Name: </label>';
+            $classNameVar = "className" . $i;
+            $classValue = isset($_POST[$classNameVar]) ? $_POST[$classNameVar] : "";
+            echo '<input type="text" name="className' . $i . '" value="' . $classValue .'"/>';
+
+            //Units label and number entry
+            echo '<label for="units' . $i . '" class="newSectionLabel">Units: </label>';
+            $unitVar = "units" . $i;
+            $unitValue = isset($_POST[$unitVar]) ? $_POST[$unitVar] : "";
+            echo '<input type="number" pattern="[0-9]" name="units' . $i .'"/ id="units1" class="unitInput" size="2" min="1" max="9" value="' . $unitValue . '"/>';
+                
+            //Grade radio buttons
+            echo '<label class="newSectionLabel">Grade: </label>';
+            makeGradeRadioButton($i, "A");
+            makeGradeRadioButton($i, "B");
+            makeGradeRadioButton($i, "C");
+            makeGradeRadioButton($i, "D");
+            makeGradeRadioButton($i, "F");
+  
+            //Weighted checkbox and label
+            //If box was checked, will remain checked
+            echo '<label class="newSectionLabel"></label>';
+            $weightedVar = "weighted" . $i;
+            echo '<input type="checkbox" name="weighted' . $i .'" id="weighted' . $i . '"';
+            if(isset($_POST[$weightedVar]))
+                echo " checked";
+            echo '/>';
+            echo '<label for="weighted' . $i .'">Weighted?</label>';
+            
+            echo "<br/>";
+        }
+        
+        echo '<br/>';
+        
+        //Button to add a class
+        echo '<input type="submit"  name="whichButton" value="Add Class"/>';
+        
+        //Adds spacing between two buttons
+        echo "&nbsp&nbsp";
+        
+        //Button to remove a class
+        echo '<input type="submit"  name="whichButton" value="Remove Class"/>';
+        
+        echo '<br/><br/>';
+        
+        displayCalculateButton();
+}
+
+function makeGradeRadioButton($i, $gradeLetter)
+{
+    echo '<input type="radio" name="grade' . $i .'" id= "gradeRadio' . $i . $gradeLetter . '" class="radioButton" value="'. $gradeLetter .'"';
+
+    $gradeVar = "grade" . $i;
+    if((isset($_POST[$gradeVar])) && $_POST[$gradeVar] == $gradeLetter)
+
+        echo " checked";
+    echo '/>';
+    echo '<label for="gradeRadio' . $i . $gradeLetter . '">' . $gradeLetter . '</label>';
+}
+
+function displayCalculateButton()
+{
+    echo '<input type="submit"  name="whichButton" value="Calculate!"/>';
+        echo '</form>';
+    echo '</fieldset>';
+}
+
+//Makes sure there are no empty fields
+//If there is something missing from any of the required fields, (calculate) submit button will be disabled
+function validateForm()
+{
+    //Resets to 0 every time for proper calculation
+    $validClass = 0;
+    
+    for($l = 0; $l < $_SESSION['numClasses']; $l++)
+    {
+        $classNameVar = "className" . $l;
+        $unitVar = "units" . $l;
+        $gradeVar = "grade" . $l;
+        
+        //Checks to see if any information is missing from the class ("Weighted" option not included as it is optional)
+        //Prints out an error message if aynthing is missing
+        if((isset($_POST[$classNameVar]) && $_POST[$classNameVar] != "") && (isset($_POST[$unitVar]) && $_POST[$unitVar] != "")
+                    && ( isset($_POST[$gradeVar]) && $_POST[$gradeVar] != ""))
+        {
+            ++$validClass;
+        }
+        //If not all three required fields are set
+        else
+        {
+            echo '<br>';
+            echo '<span class="error-message">You are missing the following: <br/>';
+            echo '&nbsp;&nbsp;&nbsp;&nbsp *Class ' . ($l+1) . ': ';
+            
+            if(!isset($_POST[$classNameVar]) || $_POST[$classNameVar] == "")
+            {
+    
+                echo "Name ";
+            }
+            
+            if(!isset($_POST[$unitVar]) || $_POST[$unitVar] == "")
+            {
+                echo "Units ";
+            }
+            
+            if(!isset($_POST[$gradeVar]) || $_POST[$gradeVar] == "")
+            {
+                echo "Grade";
+            }
+            
+            echo "</span>";
+        }
+        
+    }
+    
+    echo "<br><br>";
+    
+    if($validClass == $_SESSION['numClasses'])
+    {
+        calculateGPA();
+    }
+}
+
+function calculateGPA()
+{
+    //Resets to 0 and calculates accordingly every submission
+    //Prevents continuous adding-on-to of the cumulative unit and grade point totals
+    //(Allows user to keep hitting "Calculate" without calculation issues)
+    $_SESSION['totalUnits'] = 0;
+    $_SESSION['totalGradePoints'] = 0;
+    
+    for($k = 0; $k <$_SESSION['numClasses']; $k++)
+    {
+        $unitAccessor = "units" . $k;
+        $gradeAccessor = "grade" . $k;
+        $weightAccessor = "weighted" . $k;
+        
+        $postUnits = $_POST[$unitAccessor];
+        $postGrade = $_POST[$gradeAccessor];
+        
+        $_SESSION['totalUnits'] += $postUnits;
+
+        switch($postGrade)
+        {
+            case "A":
+                $gradeValue = 4;
+                break;
+            case "B":
+                $gradeValue = 3;
+                break;
+            case "C":
+                $gradeValue = 2;
+                break;
+            case "D":
+                $gradeValue = 1;
+                break;
+            case "F":
+                $gradeValue = 0;
+                break;
+            default:
+                break;
+        }
+
+        if(isset($_POST[$weightAccessor]))
+        {
+            ++$gradeValue;
+        }
+        
+        $_SESSION['totalGradePoints'] += ($postUnits * $gradeValue);
+        
+    }
+
+    $calculatedGPA = number_format(($_SESSION['totalGradePoints'] / $_SESSION['totalUnits']), 2);
+    
+    echo '<h1 id="gpaResult">GPA: ' . $calculatedGPA . '</h1>';
+}
+
+?>
--- homework/3/index.php
+++ homework/3/index.php
@@ -1,106 +1,64 @@
 <?php
-// $_POST['className'] = array();
+    session_start();
 
+    include 'inc/functions.php';
+
+    //FORM LOGIC
+
+    //Makes sure the number of classes never goes to 0 or into negatives
+    //Makes sure there is 1 class available on first load (empty session)
+    if($_SESSION['numClasses'] < 1)
+    {
+        $_SESSION['numClasses'] = 1;
+    }
+
+    //Handles button logic
+    $button = $_POST['whichButton'];
+    
+    if(isset($button))
+    {
+        //Adding class
+        if($button == "Add Class")
+        {
+            ++$_SESSION['numClasses'];
+        }
+        //Removing class
+        elseif($button == "Remove Class" && $_SESSION['numClasses'] > 1)
+        {
+            --$_SESSION['numClasses'];
+        }
+        //Calculating GPA
+        elseif($button == "Calculate!")
+        {
+            validateForm();
+        }
+    }
+    
 ?>
 
-<!--
-
-Radio button at end: ask if this was helpful
-    don't forget accessible label for radio button
-And then a helpful counter: "-- people found this helpful"
-    use sessions (don't forget to start session lol)
-    
--->
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Semester GPA</title>
-        <link rel="stylesheet" type="text/css" href="css/styles.css">
+        <title>GPA Calculator</title>
+        <link rel="stylesheet" type="text/css" href="styles.css">
     </head>
     <body>
-        <fieldset>
-            <legend>Semester GPA Calculator</legend>
-        <form method="post" action="results.php">
-            <!----------Dividing Line for organization---------->
-            
-            <label for="className[]">Class Name: </label>
-            <input type="text" name='className[]'/>
-            
-            <label for="units1">Units: </label>
-            <input type="number" name="units[]"/ id="units1" class="unitInput"/>
-            
-            <input type="checkbox" name="weighted[]" id="weighted1"/>
-            <label for="weighted1">Weighted?</label>
-            
-             <br/> <!----------Dividing Line for organization---------->
-            
-            <label for="className[]">Class Name: </label>
-            <input type="text" name='className[]'/>
-            
-            <label for="units2">Units: </label>
-            <input type="number" name="units[]"/ id="units2" class="unitInput"/>
-            
-            <input type="checkbox" name="weighted[]" id="weighted2"/> 
-            <label for="weighted2">Weighted?</label>
-            
-            <br/> <!----------Dividing Line for organization---------->
-            
-            <label for="className[]">Class Name: </label>
-            <input type="text" name='className[]'/>
-            
-            <label for="units3">Units: </label>
-            <input type="number" name="units[]"/ id="units3" class="unitInput"/>
-            
-            <input type="checkbox" name="weighted[]" id="weighted3"/> 
-            <label for="weighted3">Weighted?</label>
-            
-            <br/> <!----------Dividing Line for organization---------->
-            
-            <label for="className[]">Class Name: </label>
-            <input type="text" name='className[]'/> 
-            
-            <label for="units4">Units: </label>
-            <input type="number" name="units[]"/ id="units4" class="unitInput"/>
-            
-            <input type="checkbox" name="weighted[]" id="weighted4"/> 
-            <label for="weighted4">Weighted?</label>
-            
-            <br/> <!----------Dividing Line for organization---------->
-            
-            <label for="className[]">Class Name: </label>
-            <input type="text" name='className[]'/> 
-            
-            <label for="units4">Units: </label>
-            <input type="number" name="units[]"/ id="units4" class="unitInput"/>
-            
-            <input type="checkbox" name="weighted[]" id="weighted4"/> 
-            <label for="weighted4">Weighted?</label>
-            
-            <br/> <!----------Dividing Line for organization---------->
-            
-            <input type="submit" value="Calculate!"/>
-            
-        </form>
-        </fieldset>
         
+        <h1 class="header">What's Your GPA?</h1>
         
+        <!--<br/><br/>-->
+        
+        <div id="formDiv">
         <?php
-        
-        echo "class: ";
-        echo "<pre>";
-        // $numClasses = count($_POST['className']);
-        // for($x = 0; $x < count($_POST['className']) - 1; $x++)
-        
-        // foreach($_POST as $className => $class)
-            // echo $class;
-            
-        var_dump($_POST['className']);
-            
-        echo "</pre>";
-        
-        
-        
+            displayForm();
         ?>
+        </div>
+        
+        <!--<br/><br/>-->
+        
+        <div id="footer">
+            This GPA calculator is meant only to be a helpful tool. <br>
+        </div>
         
     </body>
 </html>
