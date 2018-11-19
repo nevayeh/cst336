2b0a6f690559f059cc999946bbdc00c18a24b011
Neva Yeh <nyeh@csumb.edu>
2018-11-19 08:39:24 +0000

    updated landing page with css

--- index.php
+++ index.php
@@ -1,98 +1,45 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> </title>
+        <title>CST 336</title>
+        <link href="styles.css" rel="stylesheet" type="text/css" />
     </head>
     <body>
         <div>
-            <!--
-            <h1>Assignments</h1>
-            <div>
-                <a href="./assignments/1">Assignment 1</a>
-            </div>
-            
-            <div>
-                <a href="./assignments/2">Assignment 2</a>
-            </div>
-            
-            <br/>
-            
-            <hr>
-            -->
-            
             <h1>Homework</h1>
-            <div>
-                <a href="./homework/1" target="_blank">Homework 1: HTML and CSS</a>
-            </div>
             
-            <div>
-                <a href="./homework/2" target="_blank">Homework 2: Something Interesting with PHP</a>
-            </div>
+            <a href="./homework/1" target="_blank">Homework 1: HTML and CSS</a><br/>
+            <a href="./homework/2" target="_blank">Homework 2: Something Interesting with PHP</a><br/>
+            <a href="./homework/3" target="_blank">Homework 3: HTML Forms and PHP</a><br/>
+            <a href="./homework/4" target="_blank">Homework 4: Remake of Homework 2 (or 3) in JavaScript</a>
+        </div>
             
-             <div>
-                <a href="./homework/3" target="_blank">Homework 3: HTML Forms and PHP</a>
-            </div>
-            
-            <br/><br/>
-            
+        <br/> 
         <hr>
             
-            <br/>
-            
+        <div>
             <h1>Labs</h1>
-                <div>
-                    <a href="./labs/1" target="_blank">Lab 1: Personal Portfolio</a>
-                </div>
                 
-                <div>
-                    <a href="./labs/2" target="_blank">Lab 2: Slot Machine</a>
-                </div>
-                 
-                <div>
-                    <a href="https://cst336-nevayeh-lab3.herokuapp.com/" target="_blank">Lab 3: SilverJack</a>
-                </div>
-                
-                <div>
-                    <a href="./labs/4" target="_blank">Lab 4: Image Carousel</a>
-                </div>
-                
-                <div>
-                    <a href="./labs/5" target="_blank">Lab 5: Shopping Cart</a>
-                </div>
-        
-                <div>
-                    <a href="./labs/6/welcome.php" target="_blank">Lab 6: Meme Generator</a>
-                </div>
-                
-                <div>
-                    <a href="./labs/7" target="_blank">Lab 7: Meme Generator (Now with Users, Editing, and Deleting!)</a>
-                </div>
-                
-                <div>
-                    <a href="./labs/8" target="_blank">Lab 8: Hangman</a>
-                </div>
-                
-                <br/><br/>
+            <a href="./labs/1" target="_blank">Lab 1: Personal Portfolio</a><br/>
+            <a href="./labs/2" target="_blank">Lab 2: Slot Machine</a><br/>
+            <a href="https://cst336-nevayeh-lab3.herokuapp.com/" target="_blank">Lab 3: SilverJack</a><br/>
+            <a href="./labs/4" target="_blank">Lab 4: Image Carousel</a><br/>
+            <a href="./labs/5" target="_blank">Lab 5: Shopping Cart</a><br/>
+            <a href="./labs/6/welcome.php" target="_blank">Lab 6: Meme Generator</a><br/>
+            <a href="./labs/7" target="_blank">Lab 7: Meme Generator (Now with Users, Editing, and Deleting!)</a><br/>
+            <a href="./labs/8" target="_blank">Lab 8: Hangman</a><br/>
+            <a href="./labs/9" target="_blank">Lab 9: AJAX Signup</a>
+        </div>
 
-          <hr>
+        <br/>
+        <hr>
             
-            <br/>
-            
+        <div>
             <h1>Practice Midterm</h1>
-                <div>
-                    <a href="./practice/midterm-practice1/index.php" target="_blank">Practice Midterm Program 1: Winter Vacation (PHP/HTML Forms)</a>
-                </div>
-                
-                <div>
-                    <a href="./practice/midterm-practice2/index.php" target="_blank">Practice Midterm Program 2: Reports (PHP & MySQL)</a>
-                </div>
-
-                <div>
-                    <a href="./practice/practice-midterm-with-luis/index.php" target="_blank">Letter Search</a>
-                </div>
-                
-                <br/><br/><br/>
-                
+            
+            <a href="./practice/midterm-practice1/index.php" target="_blank">Practice Midterm Program 1: Winter Vacation (PHP/HTML Forms)</a><br/>
+            <a href="./practice/midterm-practice2/index.php" target="_blank">Practice Midterm Program 2: Reports (PHP & MySQL)</a><br/>
+            <a href="./practice/practice-midterm-with-luis/index.php" target="_blank">Letter Search</a>
         </div>
     </body>
 </html>
