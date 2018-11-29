4298095176ce9147355ececfe5e5fd06b4636250
Neva Yeh <nyeh@csumb.edu>
2018-11-29 21:04:00 +0000

    (lab 9) got zip search and county list population working

--- labs/9/js/javascript.js
+++ labs/9/js/javascript.js
@@ -1,28 +1,54 @@
-alert("hi");
+function getCountyList_jQuery() {        
+    $.ajax({
+        type: "get",
+        url: "http://itcdland.csumb.edu/~milara/ajax/countyList.php",
+        dataType: "json",
+        data: { "state": $("#state").val() },
+        success: function(data,status) {
+            // alert(data);
+             
+            $("#county").html("<option> Select One </option>");
+            for (var i=0; i< data.length; i++){
+                //$("#county").append("<option>" + data["counties"][i].county + "</option>" );
+                $("#county").append("<option>" + data[i].county + "</option>");
+            }
+             
+          },
+          complete: function(data,status) { //optional, used for debugging purposes
+              //alert(status);
+        }  
+    });
+}
 
-function getCountyList()
-{        
-   console.log("inside getCountyList");
-   
-   //var stateSelected = document.getElementById("state").value;
-   var stateSelected = document.getElementById("state");
-   
-   console.log(stateSelected);
-   
-   var url = "https://www.showdeolabs.com/cors?url=http://hosting.csumb.edu/laramiguel/ajax/countyList.php?state=";
+function getZipList_jQuery() {        
+    $.ajax({
+        type: "get",
+        url: "http://itcdland.csumb.edu/~milara/ajax/cityInfoByZip.php",
+        dataType: "json",
+        data: { "zip": $("#zip").val() },
+        success: function(data,status) {
+            // alert(data);
+            // console.log(data);
+             
+            // console.log("city: " + data['city']);
+            // console.log("latitude: " + data['latitude']);
+            // console.log("longitude: " + data['longitude']);
 
-   var ajax;
-    if (window.XMLHttpRequest) {
-         ajax= new XMLHttpRequest();
-     }
+            $("#citySpan").html(data['city']);
+            $("#latSpan").html(data['latitude']);
+            $("#longSpan").html(data['longitude']);
+          },
+          complete: function(data,status) { //optional, used for debugging purposes
+              //alert(status);
+        
+        }  
+    });
+}
 
-   ajax.open("GET", url + stateSelected, true);
-   ajax.send();
+var testCount = 0;
 
-     ajax.onreadystatechange=function() {
-          if (ajax.readyState==4 && ajax.status==200) {
-                alert(ajax.responseText);  //displays value retrieved from PHP program
-           }
-      }  
-
+function submit()
+{
+    testCount += 1;
+    console.log(testCount);
 }
